7f0e242a0d3f3b13f9a7bd8ba7e736670a40d046
Нано <harytyunyanaraqsya@gmall.com>
2020-12-05 16:56:51 +0400

    LessonOneIsTho

---
+++ src/LessonOneIsTho.java
@@ -0,0 +1,13 @@
+public class LessonOneIsTho {
+    public static void main(String[] args) {
+int sum = 0;
+int a = 3;
+int b = 9;
+for (int i = a; i < b ; i++) {
+            sum+= i;
+            System.out.println(sum);
+
+        }
+    }
+
+}
