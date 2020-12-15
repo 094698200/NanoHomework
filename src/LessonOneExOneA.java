ab37d2441bd46c45a9f36e055fd5ea5f5b40cb8d
Нано <harytyunyanaraqsya@gmall.com>
2020-12-15 15:14:34 +0400

    LessonOneExOneA

---
+++ src/LessonOneExOneA.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+public class LessonOneExOneA {
+    public static void main(String[] args) throws IOException {
+       int num = 2;
+       int age = 3;
+     switch (num){
+    case 1:
+    System.out.println("num + age " );
+    break;
+case 2:
+    System.out.println("num - age");
+break;
+    case 3:
+        System.out.println("num * age");
+break;
+    case 4:
+        System.out.println("num / age");
+        break;
+                 }
+    }
+}
