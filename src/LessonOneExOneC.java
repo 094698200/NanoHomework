41b257f1c44466af8479048aef94f85fa9d47284
Нано <harytyunyanaraqsya@gmall.com>
2020-12-15 15:43:03 +0400

    LessonOneExOneC

---
+++ src/LessonOneExOneC.java
@@ -0,0 +1,25 @@
+public class LessonOneExOneC {
+    public static void main(String[] args) {
+        int weight = 27;
+        switch (weight){
+            case 1:
+            default:
+                System.out.println("килограмм");
+                break;
+            case 2:
+                System.out.println("миллиграмм");
+                break;
+
+            case 3:
+            System.out.println("грамм");
+            break;
+            case 4:
+                System.out.println(" тонна");
+                break;
+            case 5:
+                System.out.println("центнер");
+                break;
+
+        }
+    }
+}
