014b922f54c5bdc1aac9d59244303470dae75cae
Нано <harytyunyanaraqsya@gmall.com>
2020-12-24 15:45:58 +0400

    LessonTwoB

---
+++ src/LessonTwoB.java
@@ -0,0 +1,15 @@
+import Car.Car;
+
+public class LessonTwoB {
+    public static void main(String[] args) {
+        Car car = new Car();
+        String  engine;
+        String wheels;
+        String make;
+        String  speed;
+
+        System.out.println(car);
+
+
+    }
+}
