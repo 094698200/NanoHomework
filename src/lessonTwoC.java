a7059625c9efb5456ca59668ecdf307091127d06
Нано <harytyunyanaraqsya@gmall.com>
2020-12-24 20:11:03 +0400

    LessonTwoC

---
+++ src/lessonTwoC.java
@@ -0,0 +1,13 @@
+import Zoo.Zoo;
+
+public class lessonTwoC {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        String name = new String("Anna");
+        String area;
+        String Visitors;
+
+        System.out.println(name);
+    }
+}
+
