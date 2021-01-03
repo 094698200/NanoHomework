a25b13382154f20bdcd1501a5c278365da106780
Нано <harytyunyanaraqsya@gmall.com>
2021-01-03 21:27:46 +0400

    LessonsOneA

---
+++ src/LessonsOneA.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class LessonsOneA {
+    public static void main(String[] args) {
+        ArrayList<Integer> st = new ArrayList<>();
+        st.add(0);
+        st.add(1);
+        st.add(2);
+        for (Integer integer:st) {
+            System.out.println(st);
+        }
+    }
+}
