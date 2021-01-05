92fb63c00fb199bfa6e22d77f8072d48d2972892
Нано <harytyunyanaraqsya@gmall.com>
2021-01-05 15:21:45 +0400

    LessonsOneB

---
+++ src/LessonsOneB.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LessonsOneB {
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(17);
+        list.add(79);
+        list.add(76);
+        list.add(57);
+
+        System.out.println(list.size());
+    }
+}
