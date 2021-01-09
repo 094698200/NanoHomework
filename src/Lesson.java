5987f9deba6faeaf2abf484fde4579b59e4bcf6e
Нано <harytyunyanaraqsya@gmall.com>
2021-01-09 15:54:28 +0400

    Lesson

---
+++ src/Lesson.java
@@ -0,0 +1,18 @@
+
+
+
+//21-132
+
+public class Lesson {
+    public static void main(String[] args) {
+      int x = 76;
+      int n = 100;
+      int k = 100;
+      k = n;
+        if(x > 5){
+            System.out.println(x + 5);
+        }else{
+            System.out.println(x + 8);
+        }
+    }
+}
