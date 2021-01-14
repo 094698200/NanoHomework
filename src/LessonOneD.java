921ba0f60a5b85b69433f8e2975c19c7cae322f1
Нано <harytyunyanaraqsya@gmall.com>
2021-01-14 13:29:55 +0400

    Lesson LessonOneD LessonOneD

---
+++ src/LessonOneD.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class LessonOneD {
+    public static void main(String[] args) {
+
+        ArrayList<String> num = new ArrayList<>();
+        num.add("Anna");
+        num.add("Ara");
+        num.add("Masha");
+        num.add("Liza");
+        num.add("Kara");
+        num.add("Nara");
+        num.add("Lilit");
+       {
+           num.get(1);
+           System.out.println(num.get(1));//размер списка
+          //получения элемента по индексу (начинается с 0 элемента)
+            num.get(6);
+           System.out.println(num.get(6));
+        }
+
+                }
+
+        }
+
+
